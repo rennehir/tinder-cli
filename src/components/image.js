456fffc8b2a5b763baa5826f6a561055d49c4ca8
Joel Tolvanen <joel.tolvanen@xiple.io>
2020-05-22 21:52:04 +0300

    Kuvahomma eka versio

---
+++ src/components/image.js
@@ -0,0 +1,26 @@
+"use strict";
+const React = require("react");
+const PropTypes = require("prop-types");
+const { Box } = require("ink");
+const { useState } = React;
+const terminalArt = require("terminal-art");
+
+const Image = ({ url }) => {
+	const [content, setContent] = useState("Loading image...");
+
+	const img = terminalArt
+		.toAnsii(url, {
+			maxCharWidth: 60, // my terminal is only 20 characters wide
+		})
+		.then((data) => {
+			setContent(data);
+		});
+
+	return <Box>{content}</Box>;
+};
+
+Image.propTypes = {
+	url: PropTypes.string,
+};
+
+module.exports = Image;
