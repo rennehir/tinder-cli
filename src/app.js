fcc361c2f8fcf2e62f0b1fd3135456f508d103a2
rennehir <git@rennehir.fi>
2020-05-22 22:00:54 +0300

    Bestest router implementation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 "use strict";
 const React = require("react");
-const { Box, Text } = require("ink");
+const { Box, Text, useInput } = require("ink");
+
+const { useState } = React;
 
 const App = ({ profile }) => {
+	const [currentPage, setCurrentPage] = useState(1);
+	useInput((input, key) => {
+		if (input === "1") {
+			// Feed
+			setCurrentPage(1);
+		}
+		if (input === "2") {
+			// Matches
+			setCurrentPage(2);
+		}
+		if (input === "3") {
+			// Profile
+			setCurrentPage(3);
+		}
+	});
+
 	return (
 		<Box>
-			<Text>Kukkuu saatana, {profile.name || "sinä"}</Text>
+			{currentPage === 1 && <Text>Feed</Text>}
+			{currentPage === 2 && <Text>Matches</Text>}
+			{currentPage === 3 && <Text>Profile</Text>}
 		</Box>
 	);
 };
